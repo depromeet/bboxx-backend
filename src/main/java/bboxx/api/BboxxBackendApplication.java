9c6532e545c117247ca0d60b21683c34d73821ff
Kim SuBin <sb2504@naver.com>
2021-09-26 16:21:23 +0900

    [init] ✨ init setting

---
+++ src/main/java/bboxx/api/BboxxBackendApplication.java
@@ -0,0 +1,13 @@
+package bboxx.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BboxxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BboxxBackendApplication.class, args);
+	}
+
+}
