5004fabe1c52dc52a7ad398797e3ad03d67880a6
Lee-KyungSeok <leeseok8347@gmail.com>
2021-10-17 17:30:41 +0900

    [Add] ✍ Add GetPushTokenOneQuery api
    - 푸시토큰 정보 가져오는 api 추가

--- src/main/java/bboxx/application/controller/NotificationController.java
+++ src/main/java/bboxx/application/controller/NotificationController.java
@@ -3,19 +3,19 @@ package bboxx.application.controller;
 import bboxx.application.controller.dto.response.ApiResponse;
 import bboxx.application.security.AuthUserDetail;
 import bboxx.application.service.notification.DeregisterPushTokenCommandHandler;
+import bboxx.application.service.notification.GetPushTokenOneQueryHandler;
 import bboxx.application.service.notification.RegisterPushTokenCommandHandler;
 import bboxx.domain.notification.PushToken;
 import bboxx.domain.notification.command.DeregisterPushTokenCommand;
 import bboxx.domain.notification.command.RegisterPushTokenCommand;
+import bboxx.domain.notification.query.GetPushTokenOneQuery;
+import bboxx.domain.notification.querymodel.PushTokenView;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "알람 api")
 @RestController
@@ -26,6 +26,7 @@ public class NotificationController {
 
     private final RegisterPushTokenCommandHandler registerPushTokenCommandHandler;
     private final DeregisterPushTokenCommandHandler deregisterPushTokenCommandHandler;
+    private final GetPushTokenOneQueryHandler getPushTokenOneQueryHandler;
 
     @ApiOperation(value = "push 토큰을 등록한다")
     @PostMapping("/register-push-token")
@@ -45,5 +46,16 @@ public class NotificationController {
         log.info("deregisterPushToken request, authId: {}, ownerId: {}", userDetail.getId(), command.getOwnerId());
         userDetail.validateSameUser(command.getOwnerId());
         return ApiResponse.success(deregisterPushTokenCommandHandler.handle(command));
+    }
+
+    @ApiOperation(value = "push 토큰정보를 가져온다.")
+    @GetMapping("/push-tokens/{ownerId}")
+    public ApiResponse<PushTokenView> getPushTokenOne(@PathVariable Long ownerId,
+                                                      @AuthenticationPrincipal AuthUserDetail userDetail) {
+
+        log.info("getPushTokenOne request, authId: {}, ownerId: {}", userDetail.getId(), ownerId);
+        userDetail.validateSameUser(ownerId);
+        GetPushTokenOneQuery query = new GetPushTokenOneQuery(ownerId);
+        return ApiResponse.success(getPushTokenOneQueryHandler.handle(query));
     }
 }
--- src/main/java/bboxx/infrastructure/repository/JpaPushTokenRepository.java
+++ src/main/java/bboxx/infrastructure/repository/JpaPushTokenRepository.java
@@ -2,8 +2,9 @@ package bboxx.infrastructure.repository;
 
 import bboxx.domain.notification.PushToken;
 import bboxx.domain.notification.commandmodel.PushTokenRepository;
+import bboxx.domain.notification.querymodel.PushTokenReader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaPushTokenRepository extends PushTokenRepository, JpaRepository<PushToken, Long> {
+public interface JpaPushTokenRepository extends PushTokenRepository, PushTokenReader, JpaRepository<PushToken, Long> {
 
 }
