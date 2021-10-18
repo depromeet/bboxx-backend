8703f296875f13d3dad554d175ba0f460548a3b3
Kim SuBin <sb2504@naver.com>
2021-10-18 23:38:52 +0900

    [Add] ✍ Add AuthenticationPrincipal

--- src/main/java/bboxx/application/controller/EmotionController.java
+++ src/main/java/bboxx/application/controller/EmotionController.java
@@ -2,6 +2,7 @@ package bboxx.application.controller;
 
 import bboxx.application.controller.dto.response.ApiResponse;
 import bboxx.application.controller.dto.response.EmptyJsonResponse;
+import bboxx.application.security.AuthUserDetail;
 import bboxx.application.service.emotion.EmotionService;
 import bboxx.domain.emotion.command.CreateEmotionDiaryCommand;
 import bboxx.domain.emotion.command.CreateEmotionDiaryCommandInfo;
@@ -9,6 +10,7 @@ import bboxx.domain.emotion.command.FindEmotionDiaryCommandResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "감정 일기 api")
@@ -27,7 +29,9 @@ public class EmotionController {
 
     @ApiOperation(value = "감정 일기 등록 요청")
     @PostMapping("/")
-    public ApiResponse<EmptyJsonResponse> createEmotionDiary(@RequestBody CreateEmotionDiaryCommand command) {
+    public ApiResponse<EmptyJsonResponse> createEmotionDiary(@RequestBody CreateEmotionDiaryCommand command,
+                                                             @AuthenticationPrincipal AuthUserDetail userDetail) {
+        userDetail.validateSameUser(command.getMemberId());
         emotionService.create(command);
         return ApiResponse.success();
     }
