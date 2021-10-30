d38e07b086552f1359abffc23f7c2931fc7f1b50
Kim SuBin <sb2504@naver.com>
2021-10-30 23:30:28 +0900

    [Refact] ♻ Change naming FindDecibelCommandResult to GetDecibelCommand

--- src/main/java/bboxx/application/service/decibel/DecibelService.java
+++ src/main/java/bboxx/application/service/decibel/DecibelService.java
@@ -3,7 +3,7 @@ package bboxx.application.service.decibel;
 import bboxx.domain.decibel.Decibel;
 import bboxx.domain.decibel.command.CreateDecibelCommand;
 import bboxx.domain.decibel.command.CreateDecibelCommandResult;
-import bboxx.domain.decibel.command.FindDecibelCommandResult;
+import bboxx.domain.decibel.query.GetDecibelCommand;
 import bboxx.domain.exception.DomainErrorCode;
 import bboxx.domain.exception.DomainException;
 import bboxx.infrastructure.repository.JpaDecibelRepository;
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,9 +22,9 @@ public class DecibelService {
         return new CreateDecibelCommandResult(decibelRepository.save(command.toEntity()).getId());
     }
 
-    public FindDecibelCommandResult findDecibel(Long id) {
+    public GetDecibelCommand findDecibel(Long id) {
         Decibel decibel = decibelRepository.findById(id)
                 .orElseThrow(() -> new DomainException(DomainErrorCode.DECIBEL_NOT_FOUND_ERROR));
-        return new FindDecibelCommandResult(decibel.getDecibel());
+        return new GetDecibelCommand(decibel.getDecibel());
     }
 }
