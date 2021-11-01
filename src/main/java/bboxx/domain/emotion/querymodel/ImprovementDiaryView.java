0036f8545e29cb922deed1cf52420992bbc6fe2d
Kim SuBin <sb2504@naver.com>
2021-11-01 22:39:59 +0900

    [Update] ✍ Add explanation to Improvement Diary Swagger .

--- src/main/java/bboxx/domain/emotion/querymodel/ImprovementDiaryView.java
+++ src/main/java/bboxx/domain/emotion/querymodel/ImprovementDiaryView.java
@@ -1,5 +1,6 @@
 package bboxx.domain.emotion.querymodel;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,13 +10,22 @@ import java.util.List;
 @ToString
 @Getter
 public class ImprovementDiaryView {
+    @ApiModelProperty(value = "성장 일기 고유 id", example = "1")
     private Long id;
+    @ApiModelProperty(value = "성장 일기 제목", example = "Sad Day")
     private String title;
+    @ApiModelProperty(value = "성장 일기 내용", example = "I'm so Sad. Because I dropped my candy to the ground.")
     private String content;
+    @ApiModelProperty(value = "사용자 고유 id", example = "1")
     private Long memberId;
+    @ApiModelProperty(value = "감정 일기 고유 id", example = "1")
     private Long emotionDiaryId;
+    @ApiModelProperty(value = "성장 일기 작성 후 태그한 감정 카테고리 목록", example = "['sad']")
     private List<String> tags;
+    @ApiModelProperty(value = "감정 일기 작성 시간", example = "2021-11-01T23:24:52.887247")
     private LocalDateTime keptAt;
+    @ApiModelProperty(value = "감정 일기 작성 시간", example = "2021-11-01T23:24:52.887247")
     private LocalDateTime createdAt;
+    @ApiModelProperty(value = "감정 일기 업데이트 시간", example = "2021-11-01T23:24:52.887247")
     private LocalDateTime updatedAt;
 }
